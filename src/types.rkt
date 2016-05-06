62dceb2036f1376b8c5b1f3341386243e83b02ec
Matthew Chan <matt@themattchan.com>
2016-05-06 02:25:32 -0700

    ctor should have params not ret

--- src/types.rkt
+++ src/types.rkt
@@ -11,17 +11,22 @@
 ;; TODO: add contracts/types
 
 (struct struct-lit   (id vars cdtors funcs))
-(struct ctor     (id ret stmts))
+(struct ctor     (id params stmts))
 (struct dtor     (id stmts))
 (struct func     (id ret ref params stmts))
 (struct extern-func (id ret params))
-(struct extern-var (id type array))
+(struct extern-var (id type arrays))
 
 
 ;; provide function is-initialized?
-(struct var (id static type array value))
+(struct var (id static type arrays value))
 (struct val (id static type value))
 
 (struct base-t ())
 (struct struct-t ())
+
+(struct block (code))
+
+;; dims is list of ints, normalize to vector
+(struct arrays (dims))
 ;(struct rc/var      (id static type array))
